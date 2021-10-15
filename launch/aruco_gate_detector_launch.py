cf0a2e8b6004a21f030a15411d439134fce2b938
perezsaura-david <perezsaura.david@gmail.com>
2021-10-15 14:21:02 +0200

    Change default video device

--- launch/aruco_gate_detector_launch.py
+++ launch/aruco_gate_detector_launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value='drone0'),
-        DeclareLaunchArgument('video_device', default_value='/dev/video2'),
+        DeclareLaunchArgument('video_device', default_value='/dev/video0'),
         DeclareLaunchArgument('framerate', default_value='30.0'),
         DeclareLaunchArgument('image_width', default_value='1280'),
         DeclareLaunchArgument('image_height', default_value='720'),
@@ -57,7 +57,6 @@ def generate_launch_description():
             executable='static_transform_publisher',
             name='camera_link_tf',
             namespace=LaunchConfiguration('drone_id'),
-            # arguments=['0.5397', '0.4037', '0.0', '0.920504942522', '0.390730918652', '5.18419788842e-07', '1.22132126011e-06', 'base_link', 'laser_0']
             arguments=[f'{x:.2f}', f'{y:.2f}', f'{z:.2f}', f'{roll:.2f}', f'{pitch:.2f}', f'{yaw:.2f}', f'{base_link}', f'{camera_link}'],
             output='screen',
             emulate_tty=True,
